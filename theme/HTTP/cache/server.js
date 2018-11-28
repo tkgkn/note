47accf2d5ca416d92d4530747c89580215e3cbb9
c690554125 <690554125@qq.com>
2018-11-28 20:56:02 +0800

    xx

--- theme/HTTP/cache/server.js
+++ theme/HTTP/cache/server.js
@@ -15,33 +15,33 @@ const app = http.createServer((req, res) => {
     })
   }
   if (req.url === '/script.js') {
-    res.writeHead(200, {
-      'Content-Type': 'text/javascript',
-      'Cache-Control': 'max-age=10, public, no-cache', // 客户端200秒内使用缓存（在这200秒内修改返回内容，也只能看到之前的内容，因为资源地址并没有变化，会从缓存里面去读取。通常考虑使用资源hash值来让资源地址变化，这样就会重新获取资源）
-      // 'Cache-Control': 'max-age=2000000, no-cache', // 虽然设置了很长的age，但是有no-cache，使用缓存前需到服务器验证
-      // 'Last-Modified': '123',
-      // 'Etag': '777'
-    })
-    res.end('console.log("script loaded 1")');
+    // res.writeHead(200, {
+    //   'Content-Type': 'text/javascript',
+    //   'Cache-Control': 'max-age=10, public, no-cache', // 客户端200秒内使用缓存（在这200秒内修改返回内容，也只能看到之前的内容，因为资源地址并没有变化，会从缓存里面去读取。通常考虑使用资源hash值来让资源地址变化，这样就会重新获取资源）
+    //   // 'Cache-Control': 'max-age=2000000, no-cache', // 虽然设置了很长的age，但是有no-cache，使用缓存前需到服务器验证
+    //   // 'Last-Modified': '123',
+    //   // 'Etag': '777'
+    // })
+    // res.end('console.log("script loaded 1")');
 
-    // const etag = req.headers['if-none-match']
-    // if (etag === '777') {
-    //   res.writeHead(304, {
-    //     'Content-Type': 'text/javascript',
-    //     'Cache-Control': 'max-age=2000000, no-store', 
-    //     'Last-Modified': '123',
-    //     'Etag': '777'
-    //   })
-    //   res.end('你写内容也没用，304，压根不会用服务器内容')
-    // } else{
-    //   res.writeHead(200, {
-    //     'Content-Type': 'text/javascript',
-    //     'Cache-Control': 'max-age=2000000, no-store',
-    //     'Last-Modified': '123',
-    //     'Etag': '777'
-    //   })
-    //   res.end('console.log("script loaded twice")')
-    // }
+    const etag = req.headers['if-none-match']
+    if (etag === '777') {
+      res.writeHead(304, {
+        'Content-Type': 'text/javascript',
+        'Cache-Control': 'max-age=2000000, no-cache', 
+        'Last-Modified': '123',
+        'Etag': '777'
+      })
+      res.end('你写内容也没用，304，压根不会用服务器内容')
+    } else{
+      res.writeHead(200, {
+        'Content-Type': 'text/javascript',
+        'Cache-Control': 'max-age=2000000, no-cache',
+        'Last-Modified': '123',
+        'Etag': '777'
+      })
+      res.end('console.log("script loaded twice")')
+    }
   }
 })
 
--- theme/HTTP/cookie/server.js
+++ theme/HTTP/cookie/server.js
@@ -1,26 +1,26 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
 
 const app = http.createServer((req, res) => {
-  const host = req.headers.host
-  console.log(req.headers)
-  console.log(host)
-
+  const host = req.headers.host;
   if (req.url === '/') {
     fs.readFile('index.html', (err, data) => {
       if (err) {
-        console.log(err)
-        return
+        return;
       }
-      if (host === 'a.test.com') {
-        res.writeHead(200, {
-          'Content-Type': 'text/html',
-          'Set-Cookie': ['id=123; max-age=2', 'name=jack; HttpOnly']
-        })
-      }
-      res.end(data)
-    })
+      res.setHeader('Set-Cookie', ['sex=boy; expires=Wed, 28 Nov 2018 11:03:10 GMT']);
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(data);
+    });
   }
-})
+  if (req.url === '/test') {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    res.end(JSON.stringify({ a: 1 }));
+  }
+});
 
-app.listen(3333)
+app.listen(3333);
