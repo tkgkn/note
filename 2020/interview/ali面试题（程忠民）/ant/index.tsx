ca15e4af61b1514bbd252acb071f79918b955332
c690554125 <690554125@qq.com>
2020-08-04 21:36:51 +0800

    q4重写

--- 2020/interview/ali面试题（程忠民）/ant/index.tsx
+++ 2020/interview/ali面试题（程忠民）/ant/index.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './src/q1';
 import './src/q2';
 import CacheComp from './src/q3';
 import Modal from './src/q4';
 
+function ModalTest() {
+  const [modalShow, setModalShow] = useState(false);
+  return (
+    <>
+      <button
+        onClick={() => {
+          setModalShow(true);
+        }}
+      >
+        open modal
+      </button>
+      <Modal
+        show={modalShow}
+        onMounted={() => {
+          console.log('mounted');
+        }}
+        onClosed={() => {
+          console.log('closed');
+        }}
+        onClosing={() => {
+          console.log('do closing');
+        }}
+      >
+        this is content
+      </Modal>
+    </>
+  );
+}
+
 ReactDOM.render(
   <div>
     <CacheComp />
-    <Modal show={false} />
+    <ModalTest />
   </div>,
   document.getElementById('root')
 );
--- 2020/interview/ali面试题（程忠民）/ant/src/q4.tsx
+++ 2020/interview/ali面试题（程忠民）/ant/src/q4.tsx
@@ -1,30 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './q4.css';
 
 type defaultPropsType = typeof Modal.defaultProps;
 
 interface Props extends defaultPropsType {
   show: boolean;
-  hasMask: boolean;
 }
 
 function Modal(props: React.PropsWithChildren<Props>) {
   const [show, setShow] = useState(props.show);
+
+  useEffect(() => {
+    console.log(props.show);
+    setShow(props.show);
+  }, [props.show]);
+
+  useEffect(() => {
+    props.onMounted && props.onMounted();
+    return props.onClosed;
+  }, []);
+
   return (
-    <div>
-      <p>{props.children}</p>
-      <button
-        onClick={() => {
-          setShow(false);
-        }}
-      >
-        close
-      </button>
+    <div
+      className="mask"
+      style={{
+        visibility: show ? 'visible' : 'hidden'
+      }}
+    >
+      <div className="modal">
+        <p>{props.children}</p>
+        <div className="buttons">
+          <button
+            onClick={() => {
+              props.onClosing && props.onClosing();
+              setShow(false);
+            }}
+          >
+            {props.closeBtnTxt}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
 Modal.defaultProps = {
-  hasMask: false
+  hasMask: false,
+  closeBtnTxt: '关闭',
+  onClosing: () => {},
+  onMounted: () => {},
+  onClosed: () => {}
 };
 
 export default Modal;
